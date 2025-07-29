950a75d9219aeb035c3224cbde853950079b62e4
Parkchangsoo202055084 <147802729+Parkchangsoo202055084@users.noreply.github.com>
2025-07-30 02:34:21 +0900

    Add files via upload

---
+++ Front/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./my-app.css";
+function Card(props) {
+  return (
+    <div className="card">
+      <img className="product-img" src={props.image} alt="product" />
+      <div className="card-info">
+        <p className="title">{props.title}</p>
+        <p className="subtitle">{props.subtitle}</p>
+        <p className="price">
+          <strong>{props.price.toLocaleString()}원</strong>
+        </p>
+        <p className="brand">{props.brand}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
